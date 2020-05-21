fabbe4ee95264f9afb4f7a1233a003e67b2c82d7
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2020-05-21 16:53:00 +0100

    Modified App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("LOGIN", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/LOGIN/i);
+  const { getAllByText } = render(<App />);
+  const linkElement = getAllByText(/LOGIN/i);
   expect(linkElement).toBeInTheDocument();
 });
